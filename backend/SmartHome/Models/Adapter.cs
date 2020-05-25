c6ca4d2d616717ef08aa87e1af69ccde0cdafc45
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-05-25 11:41:30 +0300

    Created interface and adapter classes

---
+++ backend/SmartHome/Models/Adapter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartHome.Models
+{
+    public class Adapter : IFeature
+    {
+        public void turnOff()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurnOn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ backend/SmartHome/Models/IFeature.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartHome.Models
+{
+    interface IFeature
+    {
+        public void TurnOn();
+        public void turnOff();
+    }
+}
